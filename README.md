b83ad17f11c45e459ffb9f50d8a37a83cdfcc762
Arnav Badrish <arnavbadrish@gmail.com>
2026-08-10 15:56:43 +1000

    Changed the method of hashing, added functionality to change how strict the matching should be, and also uploaded some images for testing out of the box

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Given a set of images, aims to find duplicates (using difference hashing) and delete duplicate instances detected.
 
+Based off the following work(s):
+- <https://www.hackerfactor.com/blog/?/archives/529-Kind-of-Like-That.html>
+- <https://github.com/benhoyt/dhash>
+
 ## Requirements
 To run this program, you will need to have Python installed on the machine.
 
--- main.py
+++ main.py
@@ -2,27 +2,33 @@ import os
 from PIL import Image
 import argparse
 # Given a directory to a set of images, attempts to find duplicates within the image files.
-# Method used: dHash
+# Method used: difference hash
 
-def dHash(image: Image.ImageFile, hash_size):
+def dHash(image: Image.ImageFile, hash_size, name:str=None):
     greyed = image.resize((hash_size + 1, hash_size)).convert("L")
 
-    diff = []
+    # debug function: if you pass the name into this function, it will save the compressed image prior to hashing
+    if name: 
+        parts = name.strip('.\\').split('.')
+        greyed.save(f"{parts[-2]}_hash.{parts[-1]}")
+
+    hash1 = 0
 
     for row in range(hash_size):
         for col in range(hash_size):
             left_pixel = greyed.getpixel((col, row))
             right_pixel = greyed.getpixel((col + 1, row))
-            diff.append(left_pixel > right_pixel)
+            bit = left_pixel < right_pixel
+            hash1 = hash1 << 1 | bit
 
-    return sum([2 ** i for (i, v) in enumerate(diff) if v])
+    return hash1
 
 def is_valid_image(file_name):
     try:
         with Image.open(file_name) as img:
             img.verify()
             return True
-    except Exception as e:
+    except:
         return False
 
 def read_image_files(path, recursive=False):
@@ -57,7 +63,7 @@ def read_images_and_get_hash_dict(path, hash_size, recursive=False):
     try:
         for file in filenames:
             img = Image.open(file)
-            hash = dHash(img, hash_size)
+            hash = dHash(img, hash_size, file)
             if hash not in htf:
                 htf[hash] = []
             htf[hash].append(file)
@@ -66,31 +72,47 @@ def read_images_and_get_hash_dict(path, hash_size, recursive=False):
         print(f"Error when hashing files: {e}")
         return None
 
-def prune_duplicates(hash_to_file_map):
+def prune_duplicates(hash_to_file_map, max_dist=4):
     if not hash_to_file_map: return False
-    duplicates = [hash_to_file_map[i] for i in hash_to_file_map if len(hash_to_file_map[i]) > 1]
-    if not duplicates: return False
+    grouped_htf = {}
+
+    for i in hash_to_file_map.keys():
+        for j in grouped_htf.keys():
+            hamming = bin(i ^ j).count('1')
+            if hamming > max_dist:
+                continue
+            else:
+                grouped_htf[j].extend(hash_to_file_map[i])
+        grouped_htf[i] = hash_to_file_map[i]
+
+    #print(grouped_htf)
+            
+    duplicates = [grouped_htf[i] for i in grouped_htf if len(grouped_htf[i]) > 1]
     try:
+        cnt = 0
         for dup in duplicates:
-            for _ in range(len(dup) - 1):
-                os.remove(dup.pop())
-        return True
+            for i in range(len(dup) - 1):
+                os.remove(dup[i])
+                cnt += 1
+        # return whether any images were removed
+        return cnt > 0
     except Exception as e:
         print(f"Error when trying to prune duplicates: {e}")
         return False
 
-def deduplicate_images(path, hash_size, recursive=False):
+def deduplicate_images(path, hash_size=8, recursive=False, max_dist=4):
     mapping = read_images_and_get_hash_dict(path, hash_size, recursive)
-    return prune_duplicates(mapping)
+    return prune_duplicates(mapping, max_dist)
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('path', default='.')
-    argparser.add_argument('-hs', '--HASH_SIZE', metavar=int, default=8)
-    argparser.add_argument('-r', '--recursive', metavar=bool, default=False)
+    argparser.add_argument('path', default='.', help="Relative path to a folder to start searching for image files from")
+    argparser.add_argument('-hs', '--hash_size', metavar=int, default=8, help="Size of hashes to be used for calculating image similarity (bigger sizes are more accurate, but may impact performance for lots of images)")
+    argparser.add_argument('-r', '--recursive', action="store_true", default=False, help="Whether to search for image files in sub-folders from the path")
+    argparser.add_argument('-m', '--max_dist', metavar=int, default=0, help="Max Hamming Distance to gauge image similarity off the image hashes (setting to 0 will make this look for exact matches only)")
     args = argparser.parse_args()
 
-    print(deduplicate_images(args.path, args.HASH_SIZE, args.recursive))
+    print(deduplicate_images(args.path, int(args.hash_size), args.recursive, int(args.max_dist)))
 
 if __name__ == "__main__":
     main()
